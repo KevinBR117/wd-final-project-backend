4cf11a87a9c37266054f6eaba7cb3e218f1155dd
KevinBarboza <Kevin.barboza@evotek.com.mx>
2023-05-07 23:16:57 -0600

    relacion preguntas respuestas

--- database/migrations/2023_05_07_041536_create_preguntas_table.php
+++ database/migrations/2023_05_07_041536_create_preguntas_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('preguntas', function (Blueprint $table) {
             $table->id();
             $table->string('pregunta', 255);
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
--- database/migrations/2023_05_07_041607_create_respuestas_table.php
+++ database/migrations/2023_05_07_041607_create_respuestas_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('respuestas', function (Blueprint $table) {
             $table->id();
-            $table->integer('preguntaId');
+            $table->foreignId('preguntaId');
             $table->string('respuesta', 255);
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
